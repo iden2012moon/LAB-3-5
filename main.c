c38c2d0b1699c354ce0ba6fd75f1120e04c6812c
Niravit Siriruengtanachai <louis_niravit@homtail.com>
2020-08-16 10:24:39 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (void)
+{
+ int b,h,a;
+ float ans;
+ printf("Enter B :");
+ scanf("%d",&b);
+ printf("Enter H :");
+ scanf("%d",&h);
+ ans = 0.5*b*h;
+ printf("Area of Trangel %0.2f",ans);
+}
+
